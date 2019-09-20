e30d197987c82fbb18a91d98e81adb69bc8a039f
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-20 15:43:27 -0400

    updated main.js- added function stubs

---
+++ main.js
@@ -0,0 +1,74 @@
+let currentQuestion = 0;
+let score = 0;
+
+//starts the quiz
+function startQuiz() {
+
+}
+
+//generates the next question
+function nextQuestion() {
+
+}
+
+//renders a question
+function renderQuestion() {
+
+}
+
+//handles user option selection and submit button
+function submitOption() {
+
+}
+
+
+//gives feedback for wrong answer
+function wrongAnswer() {
+
+}
+
+//gives feedback for correct answer
+function correctAnswer() {
+
+}
+
+//updates the score in the header
+function updateScore() {
+
+}
+
+//updates the question number in the header
+function updateQuestionNumber() {
+
+}
+
+//displays the final results view
+function displayFinalResults() {
+
+}
+
+//resets the question number and score
+function resetStats() {
+
+}
+
+//restarts the quiz
+function restartQuiz() {
+
+}
+
+//Document ready function to call other functions
+function handleQuizApp() {
+    startQuiz();
+    nextQuestion();
+    renderQuestion();
+    submitOption();
+    wrongAnswer();
+    correctAnswer();
+    updateScore();
+    updateQuestionNumber();
+    displayFinalResults();
+    resetStats();
+    restartQuiz();
+}
+
